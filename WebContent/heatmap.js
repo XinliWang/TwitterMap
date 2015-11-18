072c4da5c8ae00ff7f1ae95e47cf19ab5f94269f
Xinli Wang <wxlmaple@hotmail.com>
2015-11-18 16:02:22 -0500

    view page

---
+++ WebContent/heatmap.js
@@ -0,0 +1,178 @@
+var interval = 30000;
+var map, heatmap;
+var getTweetURL = window.location.href+ "api/get/twitter";
+var crawlTweetURL = window.location.href+ "api/stop/twitter";
+
+
+var twitter, musicTwitter, newsTwitter, jobTwitter, movieTwitter, 
+	androidTwitter, amazonTwitter, gameTwitter, halloweenTwitter;
+
+function addDataIntoMap(getTweetURL) {
+	$.getJSON(getTweetURL, function(data) {
+		console.log(data);
+		$.each(data['result'], function(index, row) {
+			var category = row["category"];
+			if (category === "music") {
+				musicTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "news") {
+				newsTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "job") {
+				jobTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "movie") {
+				movieTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "android") {
+				androidTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "amazon") {
+				amazonTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "game") {
+				gameTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "halloween") {
+				halloweenTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			}
+			twitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+		});
+	});
+}
+
+function initDataIntoMap(getTweetURL) {
+	$.getJSON(getTweetURL, {crawl:"crawl"},function(data) {
+		console.log(data);
+		$.each(data['result'], function(index, row) {
+			var category = row["category"];
+			if (category === "music") {
+				musicTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "news") {
+				newsTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "job") {
+				jobTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "movie") {
+				movieTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "android") {
+				androidTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "amazon") {
+				amazonTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "game") {
+				gameTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			} else if (category === "halloween") {
+				halloweenTwitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+			}
+			twitter.push(new google.maps.LatLng(row["lat"], row["lon"]));
+		});
+	});
+}
+
+function toggleHeatmap() {
+	heatmap.setMap(heatmap.getMap() ? null : map);
+}
+
+function initialize() {
+	var mapOptions = {
+		zoom: 5,
+		center: new google.maps.LatLng(39.496291, -96.830211),
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	loadHeatmap();
+}
+
+function loadHeatmap() {
+	heatmap = new google.maps.visualization.HeatmapLayer({
+		data : twitter,
+		radius: $("#radius-slider").slider("value"),
+		opacity: $("#opacity-slider").slider("value")
+	});
+	heatmap.setMap(map);
+}
+
+function updateRealTime() {
+	musicTwitter = new google.maps.MVCArray([]);
+	newsTwitter = new google.maps.MVCArray([]);
+	gameTwitter = new google.maps.MVCArray([]);
+	jobTwitter = new google.maps.MVCArray([]);
+	androidTwitter = new google.maps.MVCArray([]);
+	movieTwitter = new google.maps.MVCArray([]);
+	amazonTwitter = new google.maps.MVCArray([]);
+	halloweenTwitter = new google.maps.MVCArray([]);
+	twitter = new google.maps.MVCArray([]);
+	addDataIntoMap(getTweetURL);
+}
+
+function startTimer() {
+	timer = setInterval(function() {
+		updateRealTime();
+	}, interval);
+}
+
+$(document).ready(function() {
+	musicTwitter = new google.maps.MVCArray([]);
+	newsTwitter = new google.maps.MVCArray([]);
+	gameTwitter = new google.maps.MVCArray([]);
+	jobTwitter = new google.maps.MVCArray([]);
+	androidTwitter = new google.maps.MVCArray([]);
+	movieTwitter = new google.maps.MVCArray([]);
+	amazonTwitter = new google.maps.MVCArray([]);
+	halloweenTwitter = new google.maps.MVCArray([]);
+	twitter = new google.maps.MVCArray([]);
+	google.maps.event.addDomListener(window, 'load', initialize);
+	initDataIntoMap(getTweetURL);
+	
+	startTimer();
+	
+	$(function() {
+		$( "#draggable" ).draggable();
+	});
+	
+	$(function() {
+		$( "#radius-slider" ).slider({
+			orientation: "horizontal",
+			range: "min",
+			min: 1,
+			max: 50,
+			value: 20,
+			slide: function(event, ui) {
+				$("#radius-label").html("radius: " + ui.value);
+				if(heatmap == null) return;
+				heatmap.set('radius', ui.value);
+			}
+		});
+		$( "#opacity-slider" ).slider({
+			orientation: "horizontal",
+			range: "min",
+			min: 0,
+			max: 100,
+			value: 50,
+			slide: function(event, ui) {
+				$("#opacity-label").html("opacity: " + ui.value/100);
+				if(heatmap == null) return;
+				heatmap.set('opacity', ui.value/100);
+			}
+		});
+	});
+	
+	$('#select').change(function() { 
+		var category = $(this).children('option:selected').val();
+		if (category == "music") {
+	    	heatmap.set('data', musicTwitter);
+	    } else if (category == "news") {
+	    	heatmap.set('data', newsTwitter);
+	    } else if (category == "job") {
+	    	heatmap.set('data', jobTwitter);
+	    } else if (category == "game") {
+	    	heatmap.set('data', gameTwitter);
+	    } else if (category == "movie") {
+	    	heatmap.set('data', movieTwitter);
+	    } else if (category == "amazon") {
+	    	heatmap.set('data', amazonTwitter);
+	    } else if (category == "android") {
+	    	heatmap.set('data', androidTwitter);
+	    } else if (category == "halloween") {
+	    	heatmap.set('data', halloweenTwitter);
+	    } else {
+	    	heatmap.set('data', twitter);
+	    }
+	});
+});
+
+window.onbeforeunload=function(){
+	$.get(crawlTweetURL,{"shutdown":"shutdown"});
+}
---
+++ WebContent/index.jsp
@@ -0,0 +1,81 @@
+<%@page language="java" import="com.aws.twitter.TwitterGet"%>
+<head>
+  <title>Twitter Heatmap</title>
+  	<style>
+	    html, body, #map-canvas {
+	      height: 100%;
+	      margin: 0px;
+	      padding: 0px;
+				font-family: 'Raleway', sans-serif;
+				z-index:1;
+	    }
+  
+		#draggable {
+			z-index:100; 
+			background-color: rgba(200,200,255,.7); 
+			width: 250px;
+			padding: 20px;
+			position:absolute;
+			top:10px;
+			left:100px;
+			cursor: move;
+			border: black 1px solid;
+		}
+		#radius-label, #opacity-label {
+			margin-top: 10px;
+		}
+		#radius-slider, #opacity-slider {
+			width:250px;
+			margin-top: 10px;
+		}
+		#project {
+			font-size: 10pt;
+			font-weight: bold;
+			margin-bottom: 10px;
+		}
+		#radius-slider .ui-slider-handle, 
+		#opacity-slider .ui-slider-handle {
+			cursor:pointer;
+		}
+  </style>  
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
+  <script src="./jquery-2.1.1.min.js"></script>
+  <script src="./heatmap.js"></script>
+  <script src="https://code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=visualization"></script>
+</head>
+
+<body>
+  <div id="map-canvas"> </div>
+
+  <div style="text-align: center;" id="draggable">
+   	
+	<h3>Keyword</h3>
+    <select id="select">
+	  <option value="all">All Twitter</option>
+      <option value="amazon">Amazon</option>
+      <option value="android">Android</option>
+	  <option value="game">Game</option>
+	  <option value="halloween">Halloween</option>
+	  <option value="job">Job</option>
+	  <option value="movie">Movie</option>
+	  <option value="music">Music</option>
+	  <option value="news">News</option>
+	</select>
+	
+	<h3>Choice</h3>
+	<div id="radius-label">radius: 20</div>
+	<div id="radius-slider"></div>
+
+	<div id="opacity-label">opacity: 1</div>
+	<div id="opacity-slider"></div>
+  </div>
+</body>
+<%-- <script type="text/javascript">
+	window.onbeforeunload = function(){
+		<% 
+		TwitterGet twitterGet = new TwitterGet();
+		twitterGet.shutdownTwitterStream();
+		%>
+	}
+</script> --%>
